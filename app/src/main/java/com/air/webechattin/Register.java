55456702b1de5fa5c156688802b00a5b3b2a8791
Andrea Kljaić <akljaicka@gmail.com>
2020-08-21 23:58:58 +0200

    Phone verification activity

--- app/src/main/java/com/air/webechattin/Register.java
+++ app/src/main/java/com/air/webechattin/Register.java
@@ -2,7 +2,9 @@ package com.air.webechattin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,7 +12,6 @@ import android.widget.TextView;
 public class Register extends AppCompatActivity {
 
     EditText username;
-    EditText password;
     Button registerButton;
     String user;
     String pass;
@@ -21,6 +22,19 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        username = (EditText)findViewById(R.id.username);
+        registerButton = (Button)findViewById(R.id.registerButton);
+        login = (TextView)findViewById(R.id.login);
+
+
+        //Firebase.setAndroidContext(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Register.this, Login.class));
+            }
+        });
 
     }
 }
