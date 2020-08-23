469aff6c3a63b96bb04db7692cc085b4ef938698
Andrea Kljaić <akljaicka@gmail.com>
2020-08-23 19:23:53 +0200

    Registration activity

--- app/src/main/java/com/air/webechattin/LoginActivity.java
+++ app/src/main/java/com/air/webechattin/LoginActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseUser;
 
@@ -11,11 +17,56 @@ public class LoginActivity extends AppCompatActivity {
 
     FirebaseUser currentUser;
 
+    Button mLoginButton;
+    Button mPhoneLoginButton;
+    EditText mUserEmail;
+    EditText mUserPassword;
+    TextView mNeedNewAccount;
+    TextView mForgetPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields();
+
+        mNeedNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendUserToRegisterActivity();
+            }
+        });
+
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AllowUserToLogin();
+            }
+        });
+    }
+
+    private void AllowUserToLogin() {
+        String email = mUserEmail.getText().toString();
+        String password = mUserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter an email...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter a password...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void InitializeFields() {
+        mLoginButton = (Button)findViewById(R.id.login_button);
+        mPhoneLoginButton = (Button)findViewById(R.id.phone_login_button);
+        mUserEmail = (EditText)findViewById(R.id.login_email);
+        mUserPassword = (EditText)findViewById(R.id.login_password);
+        mNeedNewAccount = (TextView)findViewById(R.id.need_new_account_link);
+        mForgetPassword = (TextView)findViewById(R.id.forget_password_link);
     }
 
     @Override
@@ -30,5 +81,10 @@ public class LoginActivity extends AppCompatActivity {
     private void SendUserToMainActivity() {
         Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(mainIntent);
+    }
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
--- app/src/main/java/com/air/webechattin/RegisterActivity.java
+++ app/src/main/java/com/air/webechattin/RegisterActivity.java
@@ -1,14 +1,104 @@
 package com.air.webechattin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button mCreateAccountButton;
+    EditText mUserEmail;
+    EditText mUserPassword;
+    TextView mAlreadyHaveAccount;
+
+    FirebaseAuth mAuth;
+
+    ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        InitializeFields();
+
+        mAlreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendUserToLoginActivity();
+            }
+        });
+
+        mCreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateNewAccount();
+            }
+        });
+    }
+
+    private void CreateNewAccount() {
+        String email = mUserEmail.getText().toString();
+        String password = mUserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter an email...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter a password...", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Creating new account");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        SendUserToLoginActivity();
+                        Toast.makeText(RegisterActivity.this, "Account created successfully!", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                    else {
+                        String message = task.getException().toString();
+                        Toast.makeText(RegisterActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
+    }
+
+    private void InitializeFields() {
+        mCreateAccountButton = (Button)findViewById(R.id.register_button);
+        mUserEmail = (EditText)findViewById(R.id.register_email);
+        mUserPassword = (EditText)findViewById(R.id.register_password);
+        mAlreadyHaveAccount = (TextView)findViewById(R.id.already_have_an_account_link);
+
+        loadingBar = new ProgressDialog(this);
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent registerIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(registerIntent);
     }
 }
