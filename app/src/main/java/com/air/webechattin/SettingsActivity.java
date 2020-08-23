8e195ca7e9d19ae32c7886ae57eb639b54970d6a
Andrea Kljaić <akljaicka@gmail.com>
2020-08-23 22:23:50 +0200

    Settings activity; update username and status

--- app/src/main/java/com/air/webechattin/SettingsActivity.java
+++ app/src/main/java/com/air/webechattin/SettingsActivity.java
@@ -16,8 +16,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -46,12 +49,16 @@ public class SettingsActivity extends AppCompatActivity {
 
         InitializeFields();
 
+        mUsername.setVisibility(View.INVISIBLE);
+
         mUpdateAccountSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 UpdateSettings();
             }
         });
+
+        RetrieveUserInfo();
     }
 
     private void InitializeFields() {
@@ -99,5 +106,37 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void RetrieveUserInfo() {
+        rootReference.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if ((snapshot.exists()) && (snapshot.hasChild("name") && (snapshot.hasChild("image")))){
+                    String retrieveUsername = snapshot.child("name").getValue().toString();
+                    String retrieveStatus = snapshot.child("status").getValue().toString();
+                    String retrieveImage = snapshot.child("image").getValue().toString();
+
+                    mUsername.setText(retrieveUsername);
+                    mUserStatus.setText(retrieveStatus);
+                }
+                else if((snapshot.exists()) && (snapshot.hasChild("name"))) {
+                    String retrieveUsername = snapshot.child("name").getValue().toString();
+                    String retrieveStatus = snapshot.child("status").getValue().toString();
+
+                    mUsername.setText(retrieveUsername);
+                    mUserStatus.setText(retrieveStatus);
+                }
+                else {
+                    mUsername.setVisibility(View.VISIBLE);
+                    Toast.makeText(SettingsActivity.this, "Please set & update profile info", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
