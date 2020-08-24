5b56f425e1e4182dbb2bd22a258c33d9d05671ca
Andrea Kljaić <akljaicka@gmail.com>
2020-08-25 01:10:19 +0200

    Accept/Decline request fragment

--- app/src/main/java/com/air/webechattin/RequestsFragment.java
+++ app/src/main/java/com/air/webechattin/RequestsFragment.java
@@ -3,17 +3,17 @@ package com.air.webechattin;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RequestsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RequestsFragment extends Fragment {
+
+    View requestsFragmentView;
+    RecyclerView mRequestList;
 
     public RequestsFragment() {
         // Required empty public constructor
@@ -23,6 +23,15 @@ public class RequestsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_requests, container, false);
+        requestsFragmentView = inflater.inflate(R.layout.fragment_requests, container, false);
+
+        InitializeFields();
+
+        return requestsFragmentView;
+    }
+
+    private void InitializeFields() {
+        mRequestList = (RecyclerView)requestsFragmentView.findViewById(R.id.chat_requests_list);
+        mRequestList.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
