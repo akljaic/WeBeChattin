9f912b507ae66cd0ee20f5099e7b5ced1bf22625
Andrea Kljaić <akljaicka@gmail.com>
2020-08-25 07:03:40 +0200

    Chatting; need a bugfix

--- app/src/main/java/com/air/webechattin/ChatActivity.java
+++ app/src/main/java/com/air/webechattin/ChatActivity.java
@@ -1,28 +1,38 @@
 package com.air.webechattin;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Message;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -31,15 +41,20 @@ public class ChatActivity extends AppCompatActivity {
     String receiverId;
     String receiverName;
     String receiverImage;
-
     String senderId;
 
     TextView mUserName;
     CircleImageView mUserImage;
     ImageButton mSendMessage;
     EditText mInputMessage;
+    RecyclerView mUserMessagesList;
 
     Toolbar chatToolbar;
+
+
+    final List<Messages> messagesList = new ArrayList<>();
+    LinearLayoutManager linearLayoutManager;
+    MessageAdapter messageAdapter;
 
     FirebaseAuth mAuth;
     DatabaseReference rootReference;
@@ -85,8 +100,50 @@ public class ChatActivity extends AppCompatActivity {
 
         mUserImage = (CircleImageView)findViewById(R.id.custom_profile_image);
         mUserName = (TextView)findViewById(R.id.custom_profile_name);
+
         mSendMessage = (ImageButton)findViewById(R.id.chat_send_message_button);
         mInputMessage = (EditText)findViewById(R.id.chat_input_message);
+
+        mUserMessagesList = (RecyclerView)findViewById(R.id.private_messages_list_of_users);
+        messageAdapter = new MessageAdapter(messagesList);
+        linearLayoutManager = new LinearLayoutManager(this);
+        mUserMessagesList.setLayoutManager(linearLayoutManager);
+        mUserMessagesList.setAdapter(messageAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        rootReference.child("Messages").child(senderId).child(receiverId).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Messages messages = snapshot.getValue(Messages.class);
+
+                messagesList.add(messages);
+                messageAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void SendMessage() {
@@ -115,9 +172,6 @@ public class ChatActivity extends AppCompatActivity {
             rootReference.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
                 @Override
                 public void onComplete(@NonNull Task task) {
-                    if (task.isSuccessful()){
-                        Toast.makeText(ChatActivity.this, "msg sent ", Toast.LENGTH_SHORT).show();
-                    }
                     mInputMessage.setText("");
                 }
             });
